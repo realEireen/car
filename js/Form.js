2c71e3463c2b55cb5846417ff559f3896f76cf5e
realEireen <104700347+realEireen@users.noreply.github.com>
2022-09-20 20:46:45 -0400

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,35 @@
+class Form {
+  constructor() {
+    this.input=createInput("").attribute("placeholder","enter your name")
+    this.playButton=createButton("play")
+    this.titleImg=createImg("./assets/title.png","game title")
+    this.greeting=createElement("h2")
+
+  }
+  setElementsPosition(){
+    this.titleImg.position(120,160)
+    this.input.position(width/2-110,height/2-80)
+    this.playButton.position(width/2-90,height/2-20)
+    this.greeting.position(width/2-300,height/2-100)
+  }
+
+  hide(){
+    this.greeting.hide()
+    this.playButton.hide()
+    this.input.hide()
+  }
+  handleMousePressed(){
+    this.playButton.mousePressed(() =>{
+      this.input.hide()
+      this.playButton.hide()
+      var message =
+      hello ${this.imput.value()}
+      </br> wait for another player to join
+      this.greeting.html(message)
+    })
+  }
+  display(){
+    this.setElementsPosition()
+    this.handleMousePressed()
+  }
+}
